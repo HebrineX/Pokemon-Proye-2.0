8befaa6b4e68123122b4664dfdcaebd70732a736
HebrineX <biancolucasgerman@gmail.com>
2022-10-28 11:42:35 -0300

    estructura BASE(creo que no me falta agregar nada)

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PokemonsModule } from './pokemons/pokemons.module';
+import { GymsModule } from './gyms/gyms.module';
+import { TrainersModule } from './trainers/trainers.module';
+import { GymModule } from './gym/gym.module';
+import { GymsModule } from './gyms/gyms.module';
+
+@Module({
+  imports: [PokemonsModule, GymsModule, GymModule, TrainersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
